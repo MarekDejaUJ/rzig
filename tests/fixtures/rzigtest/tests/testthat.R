b5f9cbbaab43bf8bc7e3fa05a851dd261edf35f2
MarekDejaUJ <marekdejauj@gmail.com>
2026-08-20 01:29:25 +0200

    ci: restore full Windows test suite

--- tests/fixtures/rzigtest/tests/testthat.R
+++ tests/fixtures/rzigtest/tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
 library(rzigtest)
 
-if (identical(Sys.getenv("RZIG_WINDOWS_SMOKE_ONLY"), "true")) {
-  test_dir("testthat", filter = "ucrt-smoke")
-} else {
-  test_check("rzigtest")
-}
+test_check("rzigtest")
